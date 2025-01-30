f0348ffba2267a1d16ae780f51494cbb7b7d6acd
@aleksei_neverko <aaneverko@mail.ru>
2025-01-30 19:17:47 +0300

    add implementation DAO

--- src/main/java/com/example/school/controller/GreetingController.java
+++ src/main/java/com/example/school/controller/GreetingController.java
@@ -1,19 +1,32 @@
 package com.example.school.controller;
 
+import com.example.school.dao.StudentDAO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
+@RequestMapping("/greeting")
 public class GreetingController {
 
-    @GetMapping("/greeting")
-    public String greeting(
-            @RequestParam(name="name", required=false, defaultValue="World")
-            String name, Model model) {
-            model.addAttribute("name", name);
+    private final StudentDAO studentDAO;
+    @Autowired
+    public GreetingController(StudentDAO studentDAO) {
+        this.studentDAO = studentDAO;
+    }
+
+
+    @GetMapping
+    public String greeting(Model model) {
+        model.addAttribute("person", studentDAO.getAllStudents());
         return "greeting";
+    }
+
+    @GetMapping("/{id}")
+    public String showStudents(@PathVariable("id") int id, Model model) {
+        model.addAttribute("student", studentDAO.getStudent(id));
+        return "show";
     }
 
 }
